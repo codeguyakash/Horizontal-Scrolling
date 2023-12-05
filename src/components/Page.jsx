10d7a50018554ecae7605b145ecb0517db944561
Akash <codeguyakash@gmail.com>
2023-12-05 18:49:38 +0530

    more style

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react'
 
-const Page = ({ title, para, icon, color, textColor }) => {
-  let icons = false;
+const Page = ({ title, para, icon, bgColor, textColor }) => {
   const [isImage, setIsImage] = useState(true)
   useEffect(() => {
     if (icon) {
@@ -13,11 +12,11 @@ const Page = ({ title, para, icon, color, textColor }) => {
   return (
     <>
 
-      <div className='container flex' style={{ backgroundColor: color }}>
+      <div className='container flex' style={{ backgroundColor: bgColor }}>
         <div className='content-div'>
           <div className='content'>
-            {isImage ? "" : <img src={icons} alt="" />}
-            <h1 style={{ color: textColor }}>{title}</h1>
+            {isImage ? "" : <div><img src={icon} alt="icons" /></div>}
+            <h1 style={{ color: textColor }}>{title}&#11208;</h1>
             <h2>{para}</h2>
           </div>
         </div>
--- src/components/Screen.jsx
+++ src/components/Screen.jsx
@@ -9,12 +9,13 @@ const Screen = () => {
         <>
             <SideNav />
             <div className="main-container">
-                <div className='horizontal-scroll-content'></div>
+
                 <Page title={"We build fun experiences using cutting edge technologies"} color={"gray"} icon={""} />
 
-                <Page title={"About us"} para={"We understand the importance of communication, a good and healthy communication build a better relationship. We've experience of working with startups & unicorns alike. "} icon={"https://bit.ly/41a2Er1"} color={"#464646"} textColor={"#fff"} />
+                <Page title={"About us"} para={"We understand the importance of communication, a good and healthy communication build a better relationship. We've experience of working with startups & unicorns alike. "} icon={"https://bit.ly/41a2Er1"} bgColor={"#464646"} textColor={"#ffffff"} />
 
-                <Page title={"Services"} para={"UIBIX is specialized in UI/UX, Development, Brand strategies web design, marketing and more."} icon={"https://bit.ly/4a7PizW"} color={"#7407e1"} textColor={"#fff"} />
+                <Page title={"Services"} para={"UIBIX is specialized in UI/UX, Development, Brand strategies web design, marketing and more."} icon={"https://bit.ly/4a7PizW"} bgColor={"#7407e1"} textColor={"#ffffff"} />
+
                 {/* <CustomShape/> */}
                 <div id="scroll-btn"> Scroll &#10095; </div>
             </div>
